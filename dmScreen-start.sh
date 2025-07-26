5303e4f970ff2272d32bacd1ea2442a6ed92bd05
Dominic Marcinkowski <dominic.marcinkowski@uni-due.de>
2025-07-26 18:39:09 +0200

    Update repository URL in `dmScreen-start.sh` script

--- dmScreen-start.sh
+++ dmScreen-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install the package from Git repository
-uv pip install git+https://github.com/youruser/dmScreen.git
+uv pip install git+https://github.com/Marcinkowski-D/dmScreen.git
 
 # Run the dmScreen server
 uv run dmScreen-server
