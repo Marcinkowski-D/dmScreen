4e0be5392cb03d1de07fde2816c34eaebae96aba
Dominic Marcinkowski <dominic.marcinkowski@uni-due.de>
2025-08-14 19:41:50 +0200

    Conditionally run `dhclient` in `dmScreen-start.sh` if `eth0` exists
    ### Changes:
    
    - **`dmScreen-start.sh`**:
      - Updated the script to check if the `eth0` interface exists using `ip link show eth0` before running the `dhclient eth0` command, ensuring it avoids errors when `eth0` is unavailable.
    
    This change enhances the robustness of the script by preventing unnecessary errors in environments where `eth0` is not present.

--- dmScreen-start.sh
+++ dmScreen-start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 /bin/bash stop-ap.sh
 /bin/bash start-ap.sh
-dhclient eth0
+if ip link show eth0 >/dev/null 2>&1; then
+    dhclient eth0
+fi
 # Install the package from Git repository
 git checkout .
 git pull
